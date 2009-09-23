612f9890faff0e5fe62568abe4e9512f636426b6
Jonathan Bracy <jon@kiwi.(none)>
2009-09-23 10:53:57 -0700

    removed unneccesary extentions

--- ext/string.rb
+++
@@ -1,9 +0,0 @@
-class String
-  def underscore
-    string = self.sub('::', '/')
-    string.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-    string.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-    string.sub!("-", "_")
-    string.downcase
-  end
-end
